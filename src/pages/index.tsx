5e999c6152a00b836eb6e301de5680a6b0ba9f62
aly25 <aly25@wgu.edu>
2024-02-12 07:46:16 -0500

    Fixed issues with responsive.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,47 +6,53 @@ import { Link } from "gatsby"
 const IndexPage = () => {
   return (
     <main>
-      <div className="grid grid-cols-12 mx-auto">
-        <div className="col-start-2 col-span-10">
-          <h1 className="text-8xl font-bold">Hi, <span className="text-accent-color">I'm Alice</span>,<br/>
-          a <span className="text-accent-color">web</span> developer.</h1>
+      <div className="container mx-auto my-6 md:my-12">
+        <div className="grid grid-cols-12 mx-auto">
+          <div className="col-start-2 col-span-10">
+            <h1 className="text-4xl md:text-8xl font-bold">Hi, <span className="text-accent-color">I'm Alice</span>,<br/>
+            a <span className="text-accent-color">web</span> developer.</h1>
 
-          <div className="my-16">
-            <p className="text-2xl">With over 7 years of experience, I've crafted websites for embassies, consulates, and universities in Washington, DC. Specializing in front-end development, I prioritize user-friendly interfaces. Continuously improving my software development skills, I aim to deliver innovative solutions.</p>
-          </div>
+            <section className="my-8 md:my-16">
+              <p className="text-xl md:text-2xl">With over 7 years of experience, I've crafted websites for embassies, consulates, and universities in Washington, DC. Specializing in front-end development, I prioritize user-friendly interfaces. Continuously improving my software development skills, I aim to deliver innovative solutions.</p>
+            </section>
 
-          <div className="my-40">
-            <h2 className="text-6xl text-accent-color">My Latest Work</h2>
+            <section className="my-20 md:my-40">
+              <h2 className="text-3xl md:text-6xl text-accent-color">My Latest Work</h2>
 
-            <div className="grid grid-cols-12 gap-16 mx-auto my-16">
-              <div className="col-span-4">
-                <img src={norwich} alt="Norwich University" />
-                <p className="text-center"><Link to="https://health.norwich.edu" target="_blank" className="transition hover:text-accent-color/[.75]">Norwich University 
-                <svg className="stroke-1 h-8 inline pl-4 fill-accent-color transition hover:fill-accent-color/[.75]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></Link></p>
+              <div className="grid grid-cols-12 gap-2 md:gap-16 mx-auto my-8 md:my-16">
+                <div className="col-span-12 mb-8 md:mb-0 md:col-span-4">
+                  <Link to="https://health.norwich.edu" target="_blank" className="transition hover:text-accent-color/[.75]">
+                    <img src={norwich} alt="Norwich University" />
+                    <p className="text-center md:text-md text-sm">Norwich University 
+                    <svg className="stroke-1 h-4 md:h-8 inline pl-4 fill-accent-color transition hover:fill-accent-color/[.75]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></p>
+                  </Link>
+                </div>
+                <div className="col-span-12 mb-8 md:mb-0 md:col-span-4">
+                  <Link to="https://onlinemba.wsu.edu" target="_blank" className="transition hover:text-accent-color/[.75]">
+                    <img src={washington} alt="Washington State University" />
+                    <p className="text-center md:text-md text-sm">Washington State University 
+                    <svg className="stroke-1 h-4 md:h-8 inline pl-4 fill-accent-color transition hover:fill-accent-color/[.75]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></p>
+                  </Link>
+                </div>
               </div>
-              <div className="col-span-4">
-                <img src={washington} alt="Washington State University" />
-                <p className="text-center"><Link to="https://onlinemba.wsu.edu" target="_blank" className="transition hover:text-accent-color/[.75]">Washington State University 
-                <svg className="stroke-1 h-8 inline pl-4 fill-accent-color transition hover:fill-accent-color/[.75]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></Link></p>
+            </section>
+
+            <section className="md:my-40">
+              <h2 className="text-3xl md:text-6xl text-accent-color">Contact Me</h2>
+              <p className="text-xl md:text-2xl mb-8">You can reach out to me via:</p>
+
+              <div className="flex gap-8">
+                <Link to="https://www.linkedin.com/in/alicelly/" target="_blank">
+                  <svg className="h-16 transition delay-25 fill-white hover:fill-accent-color" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+                </Link>
+                <Link to="mailto:hello@alicely.io">
+                  <svg className="h-16 transition delay-25 fill-white hover:fill-accent-color" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/></svg>
+                </Link>
+                <Link to="https://github.com/yllwjckt" target="_blank">
+                  <svg className="h-16 transition delay-25 fill-white hover:fill-accent-color" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM265.8 407.7c0-1.8 0-6 .1-11.6c.1-11.4 .1-28.8 .1-43.7c0-15.6-5.2-25.5-11.3-30.7c37-4.1 76-9.2 76-73.1c0-18.2-6.5-27.3-17.1-39c1.7-4.3 7.4-22-1.7-45c-13.9-4.3-45.7 17.9-45.7 17.9c-13.2-3.7-27.5-5.6-41.6-5.6s-28.4 1.9-41.6 5.6c0 0-31.8-22.2-45.7-17.9c-9.1 22.9-3.5 40.6-1.7 45c-10.6 11.7-15.6 20.8-15.6 39c0 63.6 37.3 69 74.3 73.1c-4.8 4.3-9.1 11.7-10.6 22.3c-9.5 4.3-33.8 11.7-48.3-13.9c-9.1-15.8-25.5-17.1-25.5-17.1c-16.2-.2-1.1 10.2-1.1 10.2c10.8 5 18.4 24.2 18.4 24.2c9.7 29.7 56.1 19.7 56.1 19.7c0 9 .1 21.7 .1 30.6c0 4.8 .1 8.6 .1 10c0 4.3-3 9.5-11.5 8C106 393.6 59.8 330.8 59.8 257.4c0-91.8 70.2-161.5 162-161.5s166.2 69.7 166.2 161.5c.1 73.4-44.7 136.3-110.7 158.3c-8.4 1.5-11.5-3.7-11.5-8zm-90.5-54.8c-.2-1.5 1.1-2.8 3-3.2c1.9-.2 3.7 .6 3.9 1.9c.3 1.3-1 2.6-3 3c-1.9 .4-3.7-.4-3.9-1.7zm-9.1 3.2c-2.2 .2-3.7-.9-3.7-2.4c0-1.3 1.5-2.4 3.5-2.4c1.9-.2 3.7 .9 3.7 2.4c0 1.3-1.5 2.4-3.5 2.4zm-14.3-2.2c-1.9-.4-3.2-1.9-2.8-3.2s2.4-1.9 4.1-1.5c2 .6 3.3 2.1 2.8 3.4c-.4 1.3-2.4 1.9-4.1 1.3zm-12.5-7.3c-1.5-1.3-1.9-3.2-.9-4.1c.9-1.1 2.8-.9 4.3 .6c1.3 1.3 1.8 3.3 .9 4.1c-.9 1.1-2.8 .9-4.3-.6zm-8.5-10c-1.1-1.5-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3c1.1 1.5 1.1 3.3 0 4.1c-.9 .6-2.6 0-3.7-1.5zm-6.3-8.8c-1.1-1.3-1.3-2.8-.4-3.5c.9-.9 2.4-.4 3.5 .6c1.1 1.3 1.3 2.8 .4 3.5c-.9 .9-2.4 .4-3.5-.6zm-6-6.4c-1.3-.6-1.9-1.7-1.5-2.6c.4-.6 1.5-.9 2.8-.4c1.3 .7 1.9 1.8 1.5 2.6c-.4 .9-1.7 1.1-2.8 .4z"/></svg>
+                </Link>
               </div>
-            </div>
-          </div>
-
-          <div className="my-40">
-            <h2 className="text-6xl text-accent-color">Contact Me</h2>
-            <p className="text-2xl mb-8">You can reach out to me via:</p>
-
-            <div className="flex gap-8">
-              <Link to="https://www.linkedin.com/in/alicelly/" target="_blank">
-                <svg className="h-16 transition delay-25 fill-white hover:fill-accent-color" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
-              </Link>
-              <Link to="mailto:hello@alicely.io">
-                <svg className="h-16 transition delay-25 fill-white hover:fill-accent-color" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/></svg>
-              </Link>
-              <Link to="https://github.com/yllwjckt" target="_blank">
-                <svg className="h-16 transition delay-25 fill-white hover:fill-accent-color" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM265.8 407.7c0-1.8 0-6 .1-11.6c.1-11.4 .1-28.8 .1-43.7c0-15.6-5.2-25.5-11.3-30.7c37-4.1 76-9.2 76-73.1c0-18.2-6.5-27.3-17.1-39c1.7-4.3 7.4-22-1.7-45c-13.9-4.3-45.7 17.9-45.7 17.9c-13.2-3.7-27.5-5.6-41.6-5.6s-28.4 1.9-41.6 5.6c0 0-31.8-22.2-45.7-17.9c-9.1 22.9-3.5 40.6-1.7 45c-10.6 11.7-15.6 20.8-15.6 39c0 63.6 37.3 69 74.3 73.1c-4.8 4.3-9.1 11.7-10.6 22.3c-9.5 4.3-33.8 11.7-48.3-13.9c-9.1-15.8-25.5-17.1-25.5-17.1c-16.2-.2-1.1 10.2-1.1 10.2c10.8 5 18.4 24.2 18.4 24.2c9.7 29.7 56.1 19.7 56.1 19.7c0 9 .1 21.7 .1 30.6c0 4.8 .1 8.6 .1 10c0 4.3-3 9.5-11.5 8C106 393.6 59.8 330.8 59.8 257.4c0-91.8 70.2-161.5 162-161.5s166.2 69.7 166.2 161.5c.1 73.4-44.7 136.3-110.7 158.3c-8.4 1.5-11.5-3.7-11.5-8zm-90.5-54.8c-.2-1.5 1.1-2.8 3-3.2c1.9-.2 3.7 .6 3.9 1.9c.3 1.3-1 2.6-3 3c-1.9 .4-3.7-.4-3.9-1.7zm-9.1 3.2c-2.2 .2-3.7-.9-3.7-2.4c0-1.3 1.5-2.4 3.5-2.4c1.9-.2 3.7 .9 3.7 2.4c0 1.3-1.5 2.4-3.5 2.4zm-14.3-2.2c-1.9-.4-3.2-1.9-2.8-3.2s2.4-1.9 4.1-1.5c2 .6 3.3 2.1 2.8 3.4c-.4 1.3-2.4 1.9-4.1 1.3zm-12.5-7.3c-1.5-1.3-1.9-3.2-.9-4.1c.9-1.1 2.8-.9 4.3 .6c1.3 1.3 1.8 3.3 .9 4.1c-.9 1.1-2.8 .9-4.3-.6zm-8.5-10c-1.1-1.5-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3c1.1 1.5 1.1 3.3 0 4.1c-.9 .6-2.6 0-3.7-1.5zm-6.3-8.8c-1.1-1.3-1.3-2.8-.4-3.5c.9-.9 2.4-.4 3.5 .6c1.1 1.3 1.3 2.8 .4 3.5c-.9 .9-2.4 .4-3.5-.6zm-6-6.4c-1.3-.6-1.9-1.7-1.5-2.6c.4-.6 1.5-.9 2.8-.4c1.3 .7 1.9 1.8 1.5 2.6c-.4 .9-1.7 1.1-2.8 .4z"/></svg>
-              </Link>
-            </div>
+            </section>
           </div>
         </div>
       </div>
